95fa852b0744ad49a9c3c57cff328fd716ac4813
Kin Lane <kinlane@gmail.com>
2022-03-03 18:33:17 -0800

    update

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const vandium = require('vandium');
 const mysql  = require('mysql');
 
 exports.handler = vandium.generic()
-    .handler( (event, context, callback) => {
+  .handler( (event, context, callback) => {
 
-  var connection = mysql.createConnection({
+    var connection = mysql.createConnection({
     host     : process.env.host,
     user     : process.env.user,
     password : process.env.password,
     database : process.env.database
-  });
+    });
 
-	connection.query('SELECT * FROM blueprints', function (error, results, fields) {
+    connection.query('SELECT * FROM blueprints', function (error, results, fields) {
 
-	callback( null, results );
-		
+    callback( null, results );
+
   });
 });
