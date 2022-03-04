f9cf689c2a9157bd6a4770a5107a11f795203d09
Kin Lane <kinlane@gmail.com>
2022-03-03 16:32:46 -0800

    add first test

---
+++ blueprints/get/index.js
@@ -0,0 +1,20 @@
+// Added to handle injection
+const vandium = require('vandium');
+const mysql  = require('mysql');
+
+exports.handler = vandium.generic()
+    .handler( (event, context, callback) => {
+
+  var connection = mysql.createConnection({
+    host     : process.env.host,
+    user     : process.env.user,
+    password : process.env.password,
+    database : process.env.database
+  });
+
+	connection.query('SELECT * FROM blueprints', function (error, results, fields) {
+
+	callback( null, results );
+		
+  });
+}
