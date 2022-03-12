2ac8490b327cffd9a7b240b56c39f213095964e8
Kin Lane <kinlane@gmail.com>
2022-03-12 11:05:25 -0800

    update tags

--- blueprints/b_id/tags/post/index.js
+++ blueprints/b_id/tags/post/index.js
@@ -12,52 +12,26 @@ exports.handler = vandium.generic()
     });
 
 
-    con.connect(function(err) {
+    connection.connect(function(err) {
       if (err) throw err;
 
-      var sql = "SELECT * FROM tags WHERE name = '" . event.name + "'";
-      con.query(sql, function (err, result, fields) {
+      var sql = "SELECT * FROM blueprints_tags WHERE blueprint_id = " + event.blueprint_id + " and tag_id = " + event.tag_id + " LIMIT 1";
+      connection.query(sql, function (err, result, fields) {
         if(result.length > 0){
-          callback( null, result ); 
+          callback( null, result[0] ); 
         }
         else{
 
-
-          var sql = 'INSERT INTO tags(';
-    
-          var total_properties = Object.keys(event).length;
-          
-          var property_count = 1;
-          for (const [key, value] of Object.entries(event)) {
-            sql += key;
-            if(property_count != total_properties){
-              sql += ',';
-            }
-            property_count++;
-          }
-            
-          sql += ')';
-      
-          sql += ' VALUES(';
-          
-          var property_count = 1;
-          for (const [key, value] of Object.entries(event)) {
-            sql += connection.escape(value);
-            if(property_count != total_properties){
-              sql += ',';
-            }
-            property_count++;
-          }
-      
-          sql += ")";
+          var sql = "INSERT INTO blueprints_tags(blueprint_id,tag_id) VALUES(" + event.blueprint_id + ", " + event.tag_id + ")";
         
           connection.query(sql, function (error, results, fields) {
         
-            var response = {};
-            response['id'] = event.id;
-            response['name'] = event.name;
-      
-            callback( null, error );
+            var inserted = {};
+            inserted.id = results.insertId;
+            inserted.blueprint_id = event.blueprint_id;
+            inserted.tag_id = event.tag_id;
+        
+            callback( null, inserted );
       
           });          
 
