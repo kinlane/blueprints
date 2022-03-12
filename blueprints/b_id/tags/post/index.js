fde3c2068f150fff5d2517fd3cf19bb75dee771f
Kin Lane <kinlane@gmail.com>
2022-03-12 10:43:09 -0800

    build tag

---
+++ blueprints/b_id/tags/post/index.js
@@ -0,0 +1,68 @@
+const vandium = require('vandium');
+const mysql  = require('mysql');
+
+exports.handler = vandium.generic()
+  .handler( (event, context, callback) => {
+
+    var connection = mysql.createConnection({
+    host     : process.env.host,
+    user     : process.env.user,
+    password : process.env.password,
+    database : process.env.database
+    });
+
+
+    con.connect(function(err) {
+      if (err) throw err;
+
+      var sql = "SELECT * FROM tags WHERE name = '" . event.name + "'";
+      con.query(sql, function (err, result, fields) {
+        if(result.length > 0){
+          callback( null, result ); 
+        }
+        else{
+
+
+          var sql = 'INSERT INTO tags(';
+    
+          var total_properties = Object.keys(event).length;
+          
+          var property_count = 1;
+          for (const [key, value] of Object.entries(event)) {
+            sql += key;
+            if(property_count != total_properties){
+              sql += ',';
+            }
+            property_count++;
+          }
+            
+          sql += ')';
+      
+          sql += ' VALUES(';
+          
+          var property_count = 1;
+          for (const [key, value] of Object.entries(event)) {
+            sql += connection.escape(value);
+            if(property_count != total_properties){
+              sql += ',';
+            }
+            property_count++;
+          }
+      
+          sql += ")";
+        
+          connection.query(sql, function (error, results, fields) {
+        
+            var response = {};
+            response['id'] = event.id;
+            response['name'] = event.name;
+      
+            callback( null, error );
+      
+          });          
+
+        }
+      });
+    });
+
+});
