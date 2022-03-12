1d93511a57e5d3fc6e75cf7092670e39dbd9012d
Kin Lane <kinlane@gmail.com>
2022-03-12 11:20:16 -0800

    add get tags

--- blueprints/b_id/tags/get/index.js
+++ blueprints/b_id/tags/get/index.js
@@ -10,24 +10,9 @@ exports.handler = vandium.generic()
     password : process.env.password,
     database : process.env.database
     });
-    
-    var page = 0;
-    if(event.page){
-      page = event.page;
-    }
-    
-    var limit = 25;
-    if(event.limit){
-      limit = event.limit;
-    }   
-    if(limit > 50){
-      limit = 50;
-    }
 
-  var sql = 'SELECT * FROM areas WHERE blueprint_id = ' + event.blueprint_id + ' LIMIT ' + event.page + ',' + event.limit;
+    var sql = 'SELECT * FROM tags t INNER JOIN blueprints_tags bt ON t.id = bt.tag_id WHERE bt.blueprint_id = ' + event.blueprint_id + ' ORDER BY t.Name';
     connection.query(sql, function (error, results, fields) {
-
     callback( null, results );
-
   });
 });
