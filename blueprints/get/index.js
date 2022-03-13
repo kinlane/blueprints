be8728b3099141c4413e39f66888fb7e0ded97c1
Kin Lane <kinlane@gmail.com>
2022-03-12 16:06:03 -0800

    add elements

--- blueprints/get/index.js
+++ blueprints/get/index.js
@@ -11,20 +11,30 @@ exports.handler = vandium.generic()
     database : process.env.database
     });
     
+    var search = 0;
+    if(event.search){
+      search = event.search;
+    }    
+    
     var page = 0;
     if(event.page){
       page = event.page;
     }
     
-    var limit = 25;
+    var limit = 5;
     if(event.limit){
       limit = event.limit;
     }   
-    if(limit > 50){
-      limit = 50;
+    if(limit > 25){
+      limit = 25;
     }
 
-  var sql = 'SELECT * FROM blueprints LIMIT ' + event.page + ',' + event.limit;
+  if(search != ''){
+    var sql = "SELECT * FROM blueprints WHERE name LIKE '%" + search + "%' LIMIT " + event.page + "," + event.limit;
+  }
+  else{
+    var sql = "SELECT * FROM blueprints LIMIT " + event.page + "," + event.limit; 
+  }
     connection.query(sql, function (error, results, fields) {
 
     callback( null, results );
